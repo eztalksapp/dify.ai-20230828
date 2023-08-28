ca430a7bebfbc7778570b00549f7ad0d2b086841
ZhengYun <44891520+eztalksapp@users.noreply.github.com>
2023-08-28 15:59:12 +0800

    Update index.ts
    添加api

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
+export const APP_ID = '631a0bab-6d78-4282-b3f7-ef1e3a7720c7'
+export const API_KEY = '<Web API Key From Dify>'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
